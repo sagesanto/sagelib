d9ddadda8afcf4d1320307e556b3ee6c6edbb66c
sagesanto <chellster1003@gmail.com>
2023-11-30 23:42:07 -0800

    args

--- scale_ref_img.py
+++ scale_ref_img.py
@@ -5,10 +5,10 @@ from image_utils import findAllIn
 
 parser = argparse.ArgumentParser(description="Scale a reference image so its average value matches the average average value of a series of frames. Useful for alignment with reference images not taken in the same run as the series.")
 
-parser.add_argument("ref_image_input_path", type=str, required=True, help="Path to the reference image fits file")
-parser.add_argument("data_dir", type=str, required=True, help="Path to the data directory")
-parser.add_argument("file_pattern", type=str, default="*fits", help="File pattern to match in the data directory. default is '*fits'")
-parser.add_argument("ref_image_output_path", type=str, required=True, help="Path to the reference image output file")
+parser.add_argument("ref_image_input_path", type=str, help="Path to the reference image fits file")
+parser.add_argument("data_dir", type=str, help="Path to the data directory")
+parser.add_argument("ref_image_output_path", type=str, help="Path to the reference image output file")
+parser.add_argument("--file_pattern", type=str, default="*fits", help="File pattern to match in the data directory. default is '*fits'")
 parser.add_argument("--max_mem_usage_mb", type=int, default=40000, help="Maximum memory usage in MB (default: 40000)")
 
 parser.add_argument("--show", action="store_true", default=False, help="Show the resulting reference image. Defaults to False")
