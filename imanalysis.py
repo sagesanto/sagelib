ec3e7297064c4001c89244924faf66bb4bf77332
sagesanto <chellster1003@gmail.com>
2023-12-13 19:43:29 -0800

    qol

--- imanalysis.py
+++ imanalysis.py
@@ -1,19 +1,16 @@
 from Frame import Frame
 import numpy as np
-from astropy.modeling import models, fitting
 from datetime import datetime
 from photutils.segmentation import deblend_sources
 from astropy.convolution import Gaussian2DKernel, convolve
 from astropy.stats import gaussian_fwhm_to_sigma
 from photutils.segmentation import detect_sources
 import pandas as pd
-from astropy.table import Table, vstack
 from photutils.segmentation import SourceCatalog
-from astropy.stats import sigma_clipped_stats, SigmaClip
-from astropy.modeling import models, fitting
-import matplotlib.pyplot as plt
+from astropy.stats import sigma_clipped_stats
 from datetime import datetime
-import os
+import os, argparse
+from tqdm import tqdm
 
 
 def Image_Segmetation(data, threshold, npixels):
@@ -41,7 +38,7 @@ def fwhm(Frame):
     data -= median
 
     npixels = 16   # number of connected pixels needed, each above threshold, for an area to qualify as a source
-    convolved_data, segm = Image_Segmetation(data, threshold, npixels) #segm will be None instead of an array of integers if no sources are detected in the image
+    convolved_data, segm = Image_Segmetation(data, threshold, npixels)
     if convolved_data is None or segm is None:
         return None
     segm_deblend = Deblending(convolved_data, segm, npixels, nlevels=8, contrast=1)
@@ -50,6 +47,7 @@ def fwhm(Frame):
     cat = SourceCatalog(data, segm_deblend, convolved_data=convolved_data)
     columns = ['label','xcentroid','ycentroid','fwhm','gini','eccentricity','orientation','kron_flux']
     table = cat.to_table(columns=columns)
+
     for i in range(len(table)):
         if table['kron_flux'][i] > 1:
             continue
@@ -58,21 +56,27 @@ def fwhm(Frame):
 
     table.sort(['kron_flux'], reverse = True)
     table = table.groups.aggregate(np.mean)
-    print('Completed frame')
-    # print(table)
-    return {"julian":timestamp,"avg_fwhm":round(table["fwhm"][0],2)}
+    return {"julian":timestamp,"avg_fwhm":table["fwhm"][0]}
 
 
 if __name__ == "__main__":
-    data_dir = "src/test"
+    parser = argparse.ArgumentParser(description="Calculate the FWHM of a series of frames")
+    parser.add_argument("data_dir", type=str, help="Path to the data directory")
+    parser.add_argument("output_path", type=str, help="Path to the output csv file")
+    args = parser.parse_args()
+
+    data_dir = args.data_dir
+    output_path = args.output_path
     dicts = []
     files = os.listdir(data_dir)
-    for file in os.listdir(data_dir):
+    for file in tqdm(files):
         if file.endswith(".fits"):
             data = Frame.from_fits(os.path.join(data_dir,file))
             if data:
                 dicts.append(fwhm(data))
+            else:
+                print(f"No dict back from frame {file}")
 
     df = pd.DataFrame(dicts)
     print(df)
-    df.to_csv("fwhms.csv")
+    df.to_csv(os.path.join(output_path,"fwhms.csv"))
