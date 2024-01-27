18b61697ea4c2548210cc257fbcde6ff93a55d29
sagesanto <chellster1003@gmail.com>
2024-01-27 14:46:18 -0800

    cleaned up align a little

--- align.py
+++ align.py
@@ -25,6 +25,7 @@ from photutils.utils import calc_total_error
 from photutils.aperture import CircularAperture, CircularAnnulus, aperture_photometry
 #from photutils.aperture import CircularAperture, CircularAnnulus, aperture_photometry, ApertureStats
 from photutils.detection import DAOStarFinder
+import argparse
 
 warnings.filterwarnings("ignore", category=wcs.FITSFixedWarning)
 warnings.filterwarnings("ignore", category=utils.exceptions.AstropyDeprecationWarning)
@@ -35,7 +36,9 @@ def align(img_dir, pattern,ref_image_path,aligned_out,target_name, also_make_com
     images_to_align = sorted(glob.glob(os.path.join(img_dir,pattern)))
     print()
     print("Aligning the following files:",images_to_align)
-
+    if ref_image_path is None:
+        ref_image_path = images_to_align[0]
+        print(f"No reference image provided. Using {ref_image_path} as reference image.")
     # for img in images_to_align:
     #     print(img, Frame.from_fits(img).img.ndim)
     identifications = alipy.ident.run(ref_image_path, images_to_align, visu=False,verbose=False,hdu=1)
@@ -71,13 +74,21 @@ def align(img_dir, pattern,ref_image_path,aligned_out,target_name, also_make_com
 #"fdb_*.fits"
 
 if __name__ == "__main__":
-    input_dir = sys.argv[1]
-    ref_img = sys.argv[2]
-    pattern = sys.argv[3]
-    out_dir = sys.argv[4]
-    target_name = sys.argv[5]
+    parser = argparse.ArgumentParser(description='Image alignment')
+    parser.add_argument('input_dir', type=str, help='directory containing images to align')
+    parser.add_argument('pattern', type=str, help='Pattern')
+    parser.add_argument('out_dir', type=str, help='Output directory')
+    parser.add_argument('target_name', type=str, help='Target name')
+    parser.add_argument('--ref_img', type=str, default=None, help='Optional path to reference image to align to. If not provided, will use first image in input_dir')
 
-    
-    ref_img = os.path.join(input_dir,ref_img)
+    args = parser.parse_args()
+
+    input_dir = args.input_dir
+    ref_img = args.ref_img
+    pattern = args.pattern
+    out_dir = args.out_dir
+    target_name = args.target_name
+
+    ref_img = os.path.join(input_dir, ref_img)
 
     align(input_dir,pattern,ref_img,out_dir,target_name)
