519e8ea69d912f84d2eaa8e168349173456a9dea
sagesanto <chellster1003@gmail.com>
2024-06-26 18:24:04 -0700

    small logging fix

--- pipeline_utils.py
+++ pipeline_utils.py
@@ -22,17 +22,19 @@ def configure_logger(name, outfile_path):
     try:
         with open(mod("logging.json"), 'r') as log_cfg:
             logging.config.dictConfig(json.load(log_cfg))
-        logger = logging.getLogger(name)
+            logger = logging.getLogger(name)
+            # set outfile of existing filehandler. need to do this instead of making a new handler in order to not wipe the formatter off
+            # NOTE RELIES ON FILE HANDLER BEING THE SECOND HANDLER
+            root_logger = logging.getLogger()
+            file_handler = root_logger.handlers[1]
+            file_handler.setStream(Path(outfile_path).open('a'))
+            # logger.addHandler(file_handler)
+
     except Exception as e:
         print(f"Can't load logging config ({e}). Using default config.")
         logger = logging.getLogger(name)
-    # set outfile of existing filehandler. need to do this instead of making a new handler in order to not wipe the formatter off
-    # NOTE RELIES ON FILE HANDLER BEING THE SECOND HANDLER
-    root_logger = logging.getLogger()
-    file_handler = root_logger.handlers[1]
-    # set the outfile
-    file_handler.setStream(Path(outfile_path).open('a'))
-    # logger.addHandler(file_handler)
+        file_handler = logging.FileHandler(outfile_path, mode="a+")
+        logger.addHandler(file_handler)
 
     return logger
 
