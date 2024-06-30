ff582559360eea2d4882eefc88041f23adfdf563
sagesanto <chellster1003@gmail.com>
2024-06-30 01:31:41 -0700

    import silliness

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
-import os
+import sys,os
+sys.path.append(os.path.dirname(__file__))
 import pipeline_db
+sys.path.remove(os.path.dirname(__file__))
 _all = [os.path.splitext(f)[0] for f in os.listdir(os.path.dirname(__file__)) if f.endswith('.py') and not f.startswith('_')]
 _all.append('pipeline_db')
 __all__ = _all
