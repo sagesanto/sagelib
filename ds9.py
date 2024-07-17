09afc9bd0f696af7e64f8c5913b11b8624cdb983
sagesanto <chellster1003@gmail.com>
2024-07-17 11:53:06 -0700

    ds9 region-generation works better. support for WCS radii and labeling

--- ds9.py
+++ ds9.py
@@ -2,37 +2,44 @@ import astropy
 import astromatic_wrapper as aw
 from argparse import ArgumentParser
 import numpy as np
+from typing import Iterable, Any
 try:
     from pipeline_utils import check_sextractor_flags, ldac_to_table
 except:
     from .pipeline_utils import check_sextractor_flags, ldac_to_table
 
 # REGION RADIUS SHOULD BE A STRING WITH " in it (ARCSEC) IF PHYSICAL IS FALSE
-def df_to_ds9(df,outname,xname,yname,physical=False,region_radius=5,color="green"):
+def df_to_ds9(df,outname,xname,yname,physical=False,region_radius=5,color="green",labels:Iterable[Any]=None):
+    if not isinstance(region_radius,str) and not physical:
+        region_radius = str(region_radius) + '\"'
     with open(outname,"w") as f:
         f.write(f'global color={color} dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1\n')
         f.write('physical\n' if physical else 'WCS\n')
         for i, row in df.iterrows():
-            f.write(f"circle({row[xname]},{row[yname]},{region_radius}) # width=2\n")
+            circle_str = f"circle({row[xname]},{row[yname]},{region_radius}) # width=2"
+            if labels is not None:
+                circle_str = circle_str + " text={" + str(labels[i]) +"}"
+            circle_str+="\n"
+            f.write(circle_str)
     print(f"Wrote {len(df.index)} regions to {outname}.")
 
-def table_to_ds9(table, outname, xname, yname, physical=False, region_radius=5,color="green"):
+def table_to_ds9(table, outname, xname, yname, physical=False, region_radius=5,color="green",labels:Iterable[Any]=None):
     df = table.to_pandas()
-    df_to_ds9(df, outname, xname, yname, physical, region_radius,color=color)
+    df_to_ds9(df, outname, xname, yname, physical, region_radius,color=color,labels=labels)
 
-def ldac_to_ds9(fits_file, outname, physical=False,bad_sextractor_flags=None,region_radius=5):
+def ldac_to_ds9(fits_file, outname, physical=False,bad_sextractor_flags=None,region_radius=5,labels:Iterable[Any]=None):
     table = ldac_to_table(fits_file)
     df = table.to_pandas()
     if bad_sextractor_flags is not None:
         df = df[df["FLAGS"].apply(lambda f: check_sextractor_flags(f,bad_flags=bad_sextractor_flags))]
-    df_to_ds9(df,outname,xname="ALPHA_J2000" if physical else "X_IMAGE", yname="DELTA_J2000" if physical else "Y_IMAGE",physical=physical,region_radius=region_radius)
+    df_to_ds9(df,outname,xname="ALPHA_J2000" if physical else "X_IMAGE", yname="DELTA_J2000" if physical else "Y_IMAGE",physical=physical,region_radius=region_radius,labels=labels)
 
-def filtered_ldac_to_ds9(fits_file, outname, physical=False,region_radius=5):
+def filtered_ldac_to_ds9(fits_file, outname, physical=False,region_radius=5,labels:Iterable[Any]=None):
     table = ldac_to_table(fits_file)
     df = table.to_pandas()
     df = df[df["FLUX_RADIUS"] > 0]
     df = df[df["FLAGS"].apply(lambda f: check_sextractor_flags(f))]
-    df_to_ds9(df,outname,xname="ALPHA_J2000" if physical else "X_IMAGE", yname="DELTA_J2000" if physical else "Y_IMAGE",physical=physical,region_radius=region_radius)
+    df_to_ds9(df,outname,xname="ALPHA_J2000" if physical else "X_IMAGE", yname="DELTA_J2000" if physical else "Y_IMAGE",physical=physical,region_radius=region_radius,labels=labels)
 
 
 if __name__ == "__main__":
